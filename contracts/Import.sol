09899c4dba49837b4176d66d2bc64fb8cdea4014
harry91198 <harshparashar1998@gmail.com>
2023-02-25 04:59:01 +0530

    initial commit

---
+++ contracts/Import.sol
@@ -0,0 +1,4 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.9;
+import "@gnosis.pm/safe-contracts/contracts/GnosisSafe.sol";
+import "@gnosis.pm/safe-contracts/contracts/proxies/GnosisSafeProxyFactory.sol";
